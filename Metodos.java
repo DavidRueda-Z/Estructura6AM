77c7e0b1b2f845e3f28902a47d98f5c0a34b43df
Sala K 302 <salak302@salasfr.itm.edu.co>
2025-02-08 07:12:42 -0500

    Primer Proyecto de grupo 6AM

---
+++ Metodos.java
@@ -0,0 +1,25 @@
+package Estructura6AM;
+
+import java.util.Scanner;
+
+public class Metodos {
+    
+    public int SolicitarDimension(){
+        Scanner sc = new Scanner(System.in);
+        int d = 0;
+        System.out.print("ingrese la dimension de la matriz"):;
+        d = sc.nextInt();
+        return d;
+    }
+    
+    public int[][] LlenarMatriz(int d)
+    {
+        int [][] m = new int[d][d];
+
+        for(int i=0; i < m.length; i++){
+            for(int j=0;j<m.length;j++){
+                
+            }
+        }
+    }
+}
