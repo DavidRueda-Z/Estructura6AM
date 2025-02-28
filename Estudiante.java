af4f393c743849face2c507516153d62c4de5c0f
DavidRueda <davidrueda322442@correo.itm.edu.co>
2025-02-28 14:23:22 -0500

    Punto estudiante

---
+++ Estudiante.java
@@ -0,0 +1,17 @@
+package Estructura6AM;
+
+import java.util.Scanner;
+
+public class Estudiante {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        MetodosEstudiante m = new MetodosEstudiante();
+
+        System.out.println("Ingrese la cantidad de estudiantes a registrar: ");
+        int d = sc.nextInt();
+
+    }
+
+}
